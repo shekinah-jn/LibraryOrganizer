31faea058be81fca10b7cb39f9738e65b622c23f
shekinah-jn <shekinahnagarasa@gmail.com>
2026-01-21 11:50:53 -0500

    Update UnknownGenreException.java

--- LibraryOrganizer/UnknownGenreException.java
+++ LibraryOrganizer/UnknownGenreException.java
@@ -1,13 +1,3 @@
-/**
- * Assignment Information:
- *
- * Shekinah Nagarasa - 40287073
- * Course: COMP249
- * Assignment #2
- * Due Date: 17 November 2025
- *
- */
-
  /**
  * The UnknownGenreException class is a custom exception that is thrown when an unknown or invalid
  * genre is encountered in the data. This exception extends the standard Exception class and is used
@@ -17,3 +7,4 @@
 public class UnknownGenreException extends Exception {
     
 }
+
