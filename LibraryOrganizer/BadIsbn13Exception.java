e6d1f4c11f0be50afa20eb98727afd8074b3b0a2
shekinah-jn <shekinahnagarasa@gmail.com>
2026-01-21 11:48:25 -0500

    Update BadIsbn13Exception.java

--- LibraryOrganizer/BadIsbn13Exception.java
+++ LibraryOrganizer/BadIsbn13Exception.java
@@ -1,13 +1,3 @@
-/**
- * Assignment Information:
- *
- * Shekinah Nagarasa - 40287073
- * Course: COMP249
- * Assignment #2
- * Due Date: 17 November 2025
- *
- */
-
  /**
  * Exception class to handle errors related to invalid 13-digit ISBN numbers.
  * This exception is thrown when a 13-digit ISBN number is detected to be invalid
@@ -16,3 +6,4 @@
 public class BadIsbn13Exception extends Exception {
     
 }
+
