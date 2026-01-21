5da730defa6b3c81f816a7f95a9e329c14a27023
shekinah-jn <shekinahnagarasa@gmail.com>
2026-01-21 11:48:40 -0500

    Update BadPriceException.java

--- LibraryOrganizer/BadPriceException.java
+++ LibraryOrganizer/BadPriceException.java
@@ -1,14 +1,4 @@
 /**
- * Assignment Information:
- *
- * Shekinah Nagarasa - 40287073
- * Course: COMP249
- * Assignment #2
- * Due Date: 17 November 2025
- *
- */
-
-/**
  * Exception class to handle errors related to invalid book prices.
  * This exception is thrown when a book's price is found to be invalid,
  * typically when it is negative or otherwise not acceptable.
@@ -16,3 +6,4 @@
 public class BadPriceException extends Exception {
     
 }
+
