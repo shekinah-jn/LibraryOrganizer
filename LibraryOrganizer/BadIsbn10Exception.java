569c8bd0daf1babb9951a1ca0c45e4060ae4eabb
shekinah-jn <shekinahnagarasa@gmail.com>
2026-01-21 11:48:04 -0500

    Update BadIsbn10Exception.java

--- LibraryOrganizer/BadIsbn10Exception.java
+++ LibraryOrganizer/BadIsbn10Exception.java
@@ -1,13 +1,3 @@
-/**
- * Assignment Information:
- *
- * Shekinah Nagarasa - 40287073
- * Course: COMP249
- * Assignment #2
- * Due Date: 17 November 2025
- *
- */
-
  /**
  * Exception class to handle errors related to invalid 10-digit ISBN numbers.
  * This exception is thrown when a 10-digit ISBN number is detected to be invalid
@@ -17,3 +7,4 @@
 public class BadIsbn10Exception extends Exception {
     
 }
+
