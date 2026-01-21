329931fdbd18c9e024f4816955a3500292b1e16c
shekinah-jn <shekinahnagarasa@gmail.com>
2026-01-21 11:50:13 -0500

    Update TooFewFieldsException.java

--- LibraryOrganizer/TooFewFieldsException.java
+++ LibraryOrganizer/TooFewFieldsException.java
@@ -1,13 +1,3 @@
-/**
- * Assignment Information:
- *
- * Shekinah Nagarasa - 40287073
- * Course: COMP249
- * Assignment #2
- * Due Date: 17 November 2025
- *
- */
-
  /**
  * The TooFewFieldsException class is a custom exception that is thrown when there are fewer fields
  * than expected in a data input. This exception extends the standard Exception class and can be used
@@ -16,3 +6,4 @@
 public class TooFewFieldsException extends Exception {
     
 }
+
